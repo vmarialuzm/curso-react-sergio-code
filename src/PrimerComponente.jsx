6de46cf4ae1e7ff4445f15d5ac4576fbdfba0b56
vmarialuz <vmarialuzm@gmail.com>
2024-01-19 16:51:26 -0500

    Estilos en React

--- src/PrimerComponente.jsx
+++ src/PrimerComponente.jsx
@@ -1,3 +1,5 @@
+import './styles/PrimerComponente.css'
+
 const string = 'Esto es un texto'
 const number = 123456
 const array = ['Curso de React', 'Youtube', 4, 100000]
@@ -5,7 +7,7 @@ const boolean = false
 const funcion = () => 'String desde funcion'
 const objeto = { nombre: 'Curso de Javascript', duracion: 4 }
 const fecha = new Date()
- 
+
 export const PrimerComponente = () => {
   return (
     <>  
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {PrimerComponente} from './PrimerComponente'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
