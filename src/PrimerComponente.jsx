e90ef8a1638197c4cdf342d668e75a88f70c140e
vmarialuz <vmarialuzm@gmail.com>
2024-01-19 16:11:00 -0500

    Variables en JSX

--- src/PrimerComponente.jsx
+++ src/PrimerComponente.jsx
@@ -1,5 +1,13 @@
+const string = 'Esto es un texto'
+const number = 123456
+const array = ['Curso de React', 'Youtube', 4, 100000]
+const boolean = false
+const funcion = () => 'String desde funcion'
+const objeto = { nombre: 'Curso de Javascript', duracion: 4 }
+const fecha = new Date()
+ 
 export const PrimerComponente = () => {
   return (
-    <h1>Hola Mundo</h1>
+    <h1>{ JSON.stringify(fecha) }</h1>
   )
 }
